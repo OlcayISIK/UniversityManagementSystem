0ee3aec7837fcb7c85787af9f183c56b030cbdf8
onurcandolu <49251112+onurcandolu@users.noreply.github.com>
2022-06-17 13:52:32 +0300

    bug fix

--- UniversityManagementSystem-Frontend/UMS.Client.Business/Shared/PageAuthenticationService.cs
+++ UniversityManagementSystem-Frontend/UMS.Client.Business/Shared/PageAuthenticationService.cs
@@ -12,7 +12,7 @@ namespace UMS.Client.Business.Shared
     {
         Dictionary<UserType, List<Pages>> AuthenticationDictionary = new Dictionary<UserType, List<Pages>>
         {
-            [UserType.Teacher] = new List<Pages> { Pages.Home, Pages.TeacherSettings, Pages.Events, Pages.TeacherCourses, Pages.Library, Pages.TeacherStudents },
+            [UserType.Teacher] = new List<Pages> { Pages.Home, Pages.TeacherSettings, Pages.Events, Pages.TeacherCourses, Pages.Library, Pages.TeacherStudents, Pages.SocialClubs },
             [UserType.Student] = new List<Pages> { Pages.Home, Pages.Chat,Pages.Events, Pages.StudentSettings, Pages.SocialClubs, Pages.Library, Pages.Courses,Pages.StudentGrades },
             [UserType.StudentRepresentative] = new List<Pages> { Pages.Home, Pages.Chat, Pages.Events, Pages.StudentSettings, Pages.Library}
 
